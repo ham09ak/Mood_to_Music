b8be38fad0fc8a0e7b0bcc7bc2c351921cb5aa1f
ham09ak <95986570+ham09ak@users.noreply.github.com>
2025-07-19 21:12:04 +0100

    Create emotion_detection_image.py

---
+++ emotion_detection_image.py
@@ -0,0 +1,7 @@
+def emotion_detection_image(image_path):
+  try:
+     emotions=DeepFace.analyze(image_path,actions=['emotion'], enforce_detection=False) # analyze methode detect emotions , age, gender, race
+     #  enforce_detection=False allows to continue even if no face is detected
+     return emotions[0]['dominant_emotion'] # return the dominant emotions
+  except Exception as e:
+    return f"Error: {str(e)}"
